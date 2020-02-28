6ff8c30e35f066f25e4ea684e5f1ea5d5ee61e6b
Vitaly Mikhailov <personal@aragas.org>
2020-02-28 23:11:25 +0300

    Init

---
+++ Extensions/HostBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Extensions.Hosting
+{
+    public static class HostBuilderExtensions
+    {
+        public static IHostBuilder UseHealthChecks(this IHostBuilder hostBuilder) =>
+            hostBuilder .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHealthCheckPublisher();
+            });
+    }
+}
---
+++ Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using App.Metrics.Health;
+using App.Metrics.Health.Builder;
+using App.Metrics.Health.Formatters.Ascii;
+using App.Metrics.Health.Formatters.Json;
+
+using Aragas.QServer.Health;
+using Aragas.QServer.Metrics.BackgroundServices;
+
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHealthCheckPublisher(this IServiceCollection services, Func<IHealthBuilder, IHealthBuilder>? additional = null)
+        {
+            services.AddSingleton<HealthCheck, CpuHealthCheck>();
+
+            var builder = new HealthBuilder()
+                .OutputHealth.Using(new HealthStatusTextOutputFormatter())
+                .OutputHealth.Using(new HealthStatusJsonOutputFormatter());
+            builder = additional?.Invoke(builder) ?? builder;
+            builder.BuildAndAddTo(services);
+
+            services.TryAddSingleton<ICpuUsageMonitor, CpuUsageMonitor>();
+            services.AddHostedService(sp => (CpuUsageMonitor) sp.GetRequiredService<ICpuUsageMonitor>());
+
+            return services;
+        }
+    }
+}
